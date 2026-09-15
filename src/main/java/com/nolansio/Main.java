8668f29d0e21657980c5a22ea9467c2481ac2d26
nolansio <nolanarsac105@gmail.com>
2026-09-15 10:53:26 +0200

    Ajout d'un argument auto

--- src/main/java/com/nolansio/Main.java
+++ src/main/java/com/nolansio/Main.java
@@ -2,13 +2,17 @@ package com.nolansio;
 
 
 public class Main {
-    static void main() {
+    static void main(String[] args) {
         Matrix matrix = new Matrix(10, 10);
 
         matrix.display();
 
+        boolean auto = args.length > 0 && args[0].equals("--auto");
+
         while (true) {
-            IO.readln();
+            if (!auto) {
+                IO.readln();
+            }
 
             matrix.check();
             matrix.update();
